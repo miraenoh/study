e865afbf1dca0aa774cbf8435595826299f12959
miraenoh <yes_future@naver.com>
2022-03-26 15:53:45 +0900

    nextjs: 영화 예제 구현

--- react/nextjs/nextjs-intro/next.config.js
+++ react/nextjs/nextjs-intro/next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+const nextConfig = {
+	reactStrictMode: true,
+	// Sample redirection
+	async redirects() {
+		return [
+			{
+				source: '/old-blog/:path*',
+				destination: '/new-blog/:path*',
+				permanent: false
+			}
+		];
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/movies/:path*',
+				destination: `https://api.themoviedb.org/3/movie/:path*?api_key=${API_KEY}`
+			}
+		];
+	}
+};
+
+module.exports = nextConfig;
